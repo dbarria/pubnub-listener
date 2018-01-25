895de6854509a08b6436cb0d158a805a6c03827e
dbarria <dbarria@letsta.lk>
2018-01-25 18:05:03 -0300

    Init files

---
+++ listener.rb
@@ -0,0 +1,2 @@
+require 'daemons'
+Daemons.run('src/pubnub-listener.rb')
---
+++ src/pubnub-listener.rb
@@ -0,0 +1,36 @@
+require 'pubnub'
+require 'ougai'
+require 'yaml'
+require 'ERB'
+require 'byebug'
+
+current_dir = File.dirname(File.expand_path(__FILE__))
+template = ERB.new File.read "#{current_dir}/../config.yml"
+config = YAML.load template.result binding
+logger = Ougai::Logger.new("#{current_dir}/../log/#{config['log_name']}")
+
+pb_logger = Logger.new("#{current_dir}/../log/pubnub.log")
+logger.level = Logger::INFO
+pubnub = Pubnub.new(
+    publish_key: config['publish_key'],
+    subscribe_key: config['subscribe_key'],
+    auth_key: config['auth_key'],
+    heartbeat:15,
+    uuid: config['uuid'],
+    ssl: config['ssl'],
+    logger: pb_logger
+)
+
+pubnub.subscribe(
+  channel_groups: config['channel_group'],
+  origin: config['origin']
+)
+
+callback = Pubnub::SubscribeCallback.new(
+  message:  ->(envelope) {
+      logger.info(envelope.result[:data][:message])
+  }
+)
+
+pubnub.add_listener(callback: callback)
+sleep
